e40e2463fdb184c116ba68d42dc5acd0309512e8
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-20 15:54:07 +0100

    singleton

---
+++ Singleton.cs
@@ -0,0 +1,20 @@
+﻿namespace DesignPatternImplementation
+{
+    public class Model
+    {
+        private static Model instance;
+        static readonly object instanceLock = new object();
+
+        private Model() { }
+
+        public static Model getModel()
+        {
+            lock (instanceLock)
+            {
+                if (instance == null)
+                    instance = new Model();
+                return instance;
+            }
+        }
+    }
+}
