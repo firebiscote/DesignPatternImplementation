42603502fe0ea4c3eef8753be721ecf84e88a01b
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-20 16:09:24 +0100

    prototype

--- Creational/Prototype.cs
+++ Creational/Prototype.cs
@@ -4,7 +4,58 @@ using System.Text;
 
 namespace DesignPatternImplementation
 {
-    class Prototype
+    public enum RecordType
     {
+        Car,
+        Person
+    }
+
+    public abstract class Record
+    {
+        public abstract Record Clone();
+    }
+
+    public class PersonRecord : Record
+    {
+        string name;
+        int age;
+
+        public override Record Clone()
+        {
+            return (Record)this.MemberwiseClone();
+        }
+    }
+
+    public class CarRecord : Record
+    {
+        string carName;
+        Guid id;
+
+        public override Record Clone()
+        {
+            CarRecord clone = (CarRecord)this.MemberwiseClone();
+            clone.id = Guid.NewGuid();
+            return clone;
+        }
+    }
+
+    public class RecordFactory
+    {
+        private static Dictionary<RecordType, Record> prototypes = new Dictionary<RecordType, Record>();
+
+        public RecordFactory()
+        {
+            prototypes.Add(RecordType.Car, new CarRecord());
+            prototypes.Add(RecordType.Person, new PersonRecord());
+        }
+        public Record CreateRecord(RecordType type)
+        {
+            return prototypes[type].Clone();
+        }
+    }
+
+    public class Prototype
+    {
+
     }
 }
