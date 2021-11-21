e3ea6bd5a7ed1450f600f5f357349e94faf22e0b
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-21 14:19:27 +0100

    Decorator

---
+++ Structural/Decorator.cs
@@ -0,0 +1,46 @@
+﻿/*Un décorateur permet d'attacher dynamiquement de nouveaux 
+ * comportements ou responsabilités à un objet. Les décorateurs 
+ * offrent une alternative assez souple à l'héritage pour composer 
+ * de nouvelles fonctionnalités.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternImplementation.Structural
+{
+    public interface ICar
+    {
+        public double Price { get; }
+    }
+
+    public class AstonMartin : ICar
+    {
+        double ICar.Price { get { return 999.99; } }
+    }
+
+    public class Option : ICar
+    {
+        protected ICar original;
+        protected double optionPrice;
+
+        public Option(ICar original, double optionPrice)
+        {
+            this.original = original;
+            this.optionPrice = optionPrice;
+        }
+
+        double ICar.Price { get { return original.Price + optionPrice; } }
+    }
+
+    public class AirConditioner : Option
+    {
+        public AirConditioner(ICar original) : base(original, 1.0) { }
+    }
+
+    public class Parachute : Option
+    {
+        public Parachute (ICar original) : base(original, 10.0) { }
+    }
+}
---
+++ Structural/Flyweight.cs
@@ -0,0 +1,23 @@
+﻿/*Le patron poids-mouche est l'approche pour utiliser de telles classes. 
+ * D'une part la classe avec ses données internes qui la rendent unique, 
+ * et d'autre part les données externes passées à la classe en tant qu'arguments. 
+ * Ce modèle est très pratique pour des petites classes très simples. 
+ * Par exemple pour représenter des caractères ou des icônes à l'écran, 
+ * ce type de patron de conception est apprécié. Ainsi, chaque caractère 
+ * peut être représenté par une instance d'une classe contenant sa police, 
+ * sa taille, etc. La position des caractères à afficher est stockée 
+ * en dehors de cette classe. Ainsi, on a une et une seule instance 
+ * de la classe par caractère et non une instance par caractère affiché 
+ * à l'écran.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternImplementation.Structural
+{
+    class Flyweight
+    {
+    }
+}
