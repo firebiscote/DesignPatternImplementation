25d8d0d3fad09f35aac5aff6a4afcf7acbce3829
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-21 13:56:10 +0100

    Composite

---
+++ Structural/Composite.cs
@@ -0,0 +1,51 @@
+﻿/*Dans ce patron de conception, un objet composite est 
+ * constitué d'un ou de plusieurs objets similaires 
+ * (ayant des fonctionnalités similaires). 
+ * L'idée est de manipuler un groupe d'objets de la même 
+ * façon que s'il s'agissait d'un seul objet. Les objets 
+ * ainsi regroupés doivent posséder des opérations communes, 
+ * c'est-à-dire un "dénominateur commun".
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternImplementation.Structural
+{
+    public abstract class Component
+    {
+        public int Level = 0;
+        public string Name;
+
+        public virtual void Draw()
+        {
+            for (int i = 0; i < Level; i++)
+                Console.Write("\t");
+        }
+    }
+
+    public class Sheet : Component
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Sheet : " + Name);
+        }
+    }
+
+    public class Composite : Component
+    {
+        public Component[] Components;
+
+        public override void Draw()
+        {
+            base.Draw();
+            Console.WriteLine("Composite : " + Name);
+            foreach (Component component in Components)
+            {
+                component.Level = this.Level + 1;
+                component.Draw();
+            }
+        }
+    }
+}
