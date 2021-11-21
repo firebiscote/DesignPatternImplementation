1fa187cea0110f5d8ab867320c293a3a965716bd
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-21 13:03:28 +0100

    bridge

---
+++ Structural/Bridge.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace DesignPatternImplementation.Structural
+{
+    public interface IDrawingAPI
+    {
+        public void DrawCircle(double x, double y, double radius);
+    }
+
+    public class DrawingAPI2D : IDrawingAPI
+    {
+        void IDrawingAPI.DrawCircle(double x, double y, double radius)
+        {
+            Console.WriteLine("API1.circle pos " + x + ":" + y + " rad " + radius);
+        }
+    }
+
+    public class DrawingAPI2DIso : IDrawingAPI
+    {
+        void IDrawingAPI.DrawCircle(double x, double y, double radius)
+        {
+            Console.WriteLine("API2.circle pos " + x + ":" + y + " rad " + radius);
+        }
+    }
+
+    public interface IShape
+    {
+        public void Draw();
+        public void ResizeByPercentage(double pct);
+    }
+
+    public class CircleShape : IShape
+    {
+        private double x, y, radius;
+        private IDrawingAPI drawingAPI;
+
+        public CircleShape(double x, double y, double radius, IDrawingAPI drawingAPI)
+        {
+            this.x = x;
+            this.y = y;
+            this.radius = radius;
+            this.drawingAPI = drawingAPI;
+        }
+
+        void IShape.Draw()
+        {
+            drawingAPI.DrawCircle(x, y, radius);
+        }
+
+        void IShape.ResizeByPercentage(double pct)
+        {
+            radius *= pct;
+        }
+    }
+}
