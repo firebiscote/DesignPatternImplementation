a625fd6c6fda6f98b81808bae43d6b2e6f859cce
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-22 10:57:33 +0100

    command

---
+++ Behavior/Command.cs
@@ -0,0 +1,86 @@
+﻿/*Commande est un patron de conception de type comportemental 
+ * qui encapsule la notion d'invocation. Il permet de séparer 
+ * complètement le code initiateur de l'action, du code de 
+ * l'action elle-même. Ce patron de conception est souvent 
+ * utilisé dans les interfaces graphiques où, par exemple, 
+ * un item de menu peut être connecté à différentes Commandes 
+ * de façons à ce que l'objet d'item de menu n'ait pas besoin 
+ * de connaître les détails de l'action effectuée par la Commande.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatternImplementation.Behavior
+{
+    public class Switch
+    {
+        private ICommand flipUpCommand;
+        private ICommand flipDownCommand;
+
+        public Switch(ICommand flipUpCommand, ICommand flipDownCommand)
+        {
+            this.flipDownCommand = flipDownCommand;
+            this.flipUpCommand = flipUpCommand;
+        }
+
+        public void FlipUp()
+        {
+            flipUpCommand.Execute();
+        }
+
+        public void FlipDown()
+        {
+            flipDownCommand.Execute();
+        }
+    }
+
+    public class Light
+    {
+        public void TurnOn()
+        {
+            Console.WriteLine("The light is on !");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("The light is off !");
+        }
+    }
+
+    public interface ICommand
+    {
+        public void Execute();
+    }
+
+    public class TurnOnCommand : ICommand
+    {
+        private Light light;
+
+        public TurnOnCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            light.TurnOn();
+        }
+    }
+
+    public class TurnOffCommand : ICommand
+    {
+        private Light light;
+
+        public TurnOffCommand(Light light)
+        {
+            this.light = light;
+        }
+
+        public void Execute()
+        {
+            light.TurnOff();
+        }
+    }
+}
