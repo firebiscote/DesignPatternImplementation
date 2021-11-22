19b59a10be303e8f5d3360e9e72cbb82acc2c88b
Maxime CAVARROC <maxime.cavarroc@viacesi.fr>
2021-11-22 16:52:54 +0100

    Visitor

---
+++ Behavior/Visitor.cs
@@ -0,0 +1,131 @@
+﻿/*Le visiteur est une manière de séparer un algorithme d'une structure 
+ * de données. Un visiteur possède une méthode par type d'objet traité. 
+ * Pour ajouter un nouveau traitement, il suffit de créer une nouvelle 
+ * classe dérivée de la classe Visiteur. On n'a donc pas besoin de 
+ * modifier la structure des objets traités, contrairement à ce qu'il 
+ * aurait été obligatoire de faire si on avait implémenté les traitements 
+ * comme des méthodes de ces objets.
+ */
+
+using System;
+
+namespace DesignPatternImplementation.Behavior
+{
+    public interface ICarElementVisitor
+    {
+        public void Visit(Wheel wheel);
+        public void Visit(Engine engine);
+        public void Visit(Body body);
+        public void Visit(Car car);
+    }
+
+    public interface ICarElement
+    {
+        public void Accept(ICarElementVisitor visitor);
+    }
+
+    public class Wheel : ICarElement
+    {
+        public string Name { get; set; }
+
+        public Wheel(string name)
+        {
+            this.Name = name;
+        }
+
+        void ICarElement.Accept(ICarElementVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Engine : ICarElement
+    {
+        void ICarElement.Accept(ICarElementVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Body : ICarElement
+    {
+        void ICarElement.Accept(ICarElementVisitor visitor)
+        {
+            visitor.Visit(this);
+        }
+    }
+
+    public class Car
+    {
+        public ICarElement[] Elements { get; }
+
+        public Car()
+        {
+            this.Elements = new ICarElement[]
+            {
+                new Wheel("front left"),
+                new Wheel("front right"),
+                new Wheel("back left"),
+                new Wheel("back right"),
+                new Body(),
+                new Engine()
+            };
+        }
+    }
+
+    public class CarElementPrintVisitor : ICarElementVisitor
+    {
+        void ICarElementVisitor.Visit(Wheel wheel)
+        {
+            Console.WriteLine("Visiting " + wheel.Name + " wheel");
+        }
+
+        void ICarElementVisitor.Visit(Engine engine)
+        {
+            Console.WriteLine("Visiting engine");
+        }
+
+        void ICarElementVisitor.Visit(Body body)
+        {
+            Console.WriteLine("Visiting body");
+        }
+
+        void ICarElementVisitor.Visit(Car car)
+        {
+            Console.WriteLine("\nVisiting car");
+            foreach (ICarElement element in car.Elements)
+            {
+                element.Accept(this);
+            }
+            Console.WriteLine("Car visited");
+        }
+    }
+
+    public class CarElementDoVisitor : ICarElementVisitor
+    {
+        void ICarElementVisitor.Visit(Wheel wheel)
+        {
+            Console.WriteLine("Do with wheel");
+        }
+
+        void ICarElementVisitor.Visit(Engine engine)
+        {
+            Console.WriteLine("Do with engine");
+        }
+
+        void ICarElementVisitor.Visit(Body body)
+        {
+            Console.WriteLine("Do with body");
+        }
+
+        void ICarElementVisitor.Visit(Car car)
+        {
+            Console.WriteLine("\nStarting my car");
+            foreach (ICarElement element in car.Elements)
+            {
+                element.Accept(this);
+            }
+            Console.WriteLine("Car started");
+        }
+    }
+}
